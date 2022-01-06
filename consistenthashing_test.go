97f63d3216b0db6dc8bca2948854eed38dd4df59
yuwnloyblog <yuwnloyblog@163.com>
2022-01-06 11:26:56 +0800

    add test for consistenthashing

---
+++ consistenthashing_test.go
@@ -0,0 +1,32 @@
+package commonutils
+
+import (
+	"fmt"
+	"strconv"
+	"sync"
+	"testing"
+)
+
+func TestNewConsistentHash(t *testing.T) {
+	hashRing := NewConsistentHash(true)
+
+	hashRing.Add("aaa", "aaaaa", 1)
+	hashRing.Add("bbb", "bbb", 1)
+	hashRing.Add("ccc", "ccc", 1)
+
+	m := sync.Map{}
+
+	for i := 0; i < 100000; i++ {
+		nod := hashRing.Get("abc" + strconv.Itoa(i))
+
+		act, isExt := m.LoadOrStore(nod.Name, 1)
+		if isExt {
+			m.Store(nod.Name, act.(int)+1)
+		}
+	}
+	m.Range(func(key, value interface{}) bool {
+		fmt.Println(key, "", value)
+		return true
+	})
+
+}
